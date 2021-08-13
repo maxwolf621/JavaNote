fc78d28c7ed9b5d5ac70ea9047c997c5fc67f32c
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2021-08-13 23:49:35 +0800

    Create Java8Advanced.java
    ForEach for java8

---
+++ Java8Advanced.java
@@ -0,0 +1,33 @@
+/**
+  * <p> for each </p>
+  */
+public class Main {
+     public static void main(String[] args) {
+ 
+         List<String> list =  Arrays.asList("matt","john","gary");
+ 
+        // for loop
+         for(int i = 0 ; i < list.size() ; i++ ) {
+             System.out.println(list.get(i));
+         }
+ 
+         // for-each loop
+         for(String s : list) {
+             System.out.println(s);
+         }
+ 
+         //Java 8 forEach()
+         list.forEach(new Consumer<String>() {
+             @Override
+             public void accept(String s) {
+                 System.out.println(s);
+             }
+         });
+ 
+         // Java 8 forEach() Lambda
+         list.forEach(s -> System.out.println(s));
+ 
+         // Java 8 forEach() Lambda Method References
+         list.forEach(System.out::println);
+     }
+ }
