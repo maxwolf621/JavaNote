0a2b72d664d268ea48ff7e172cc19e2aa1bbbef5
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2021-08-13 23:57:20 +0800

    Update Java8AdvancedMethod.java

--- Java8AdvancedMethod.java
+++ Java8AdvancedMethod.java
@@ -32,3 +32,58 @@ public class Main {
      }
  }
 
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+         
+    }
+    
+}
+
+/**
+  * <p> Reference Method </p>
+  * <pre> ClassName :: method </p>
+  */
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> strList = Arrays.asList("A","B","C");
+
+        // Anonymous class
+        strList.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.print(s); // ABC
+            }
+        });
+
+        // Lambda expression
+        strList.forEach(s -> System.out.print(s)); // ABC
+
+        // Method reference
+        strList.forEach(System.out::print); // ABC
+
+        // Method reference(方法參考) 使用靜態方法
+        strList.forEach(Main::static_printList); // ABC
+
+        // Method reference(方法參考) 使用實例方法
+        Main main = new Main();
+        strList.forEach(main::instance_printList); // ABC
+
+    }
+
+    private static void static_printList(String s) {
+        System.out.print(s);
+    }
+
+    private void instance_printList(String s) {
+        System.out.print(s);
+    }
+
+}
